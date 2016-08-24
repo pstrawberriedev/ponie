5562af702f916cae927f4241d238213f41d05d55
pstrawberriedev <pstrawberriedev@gmail.com>
2016-08-24 12:00:13 -0600

    dskfj

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 // Set up global object
 var global = {
   
-  title:"Scratch",
-  desc: "A node express quickstart"
+  title:"ponie.club",
+  desc: ""
   
 };
 
@@ -14,18 +14,6 @@ router.get('/', function(req, res) {
 	
   
   res.render('home', { 
-    page: 'Home',
-    global: global
-  });
-  
-  
-});
-
-/* GET page */
-router.get('/page', function(req, res) {
-	
-  
-  res.render('content/page', { 
     page: 'Home',
     global: global
   });
